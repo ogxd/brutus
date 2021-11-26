0a1cacee6872f33ff9e7ede3b1c2b1998f25039f
Olivier Giniaux <oginiaux@gmail.com>
2021-11-26 22:31:28 +0100

    Add switch to render or not (rendering is slower)

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # Une PS5 pour Noël
 
 Python + Chrome --headless = une PS5 pour noël
-
-## MacOS
-
-- Installer chrome
-- Tweaker le .yaml pour la listes sites a scrap et les criteres
-- Lancer et attendre. Bien sur il faudra etre reactif quand ca sonnera, le bot n'achetera pas la PS5 a votre place :p
-
-## Windows
-
-Tout pareil que macos, sauf qu'il faudra changer le path vers chrome
--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-import os, subprocess, yaml, time, asyncio
+import os, subprocess, yaml, time, asyncio, sys, requests, urllib
+from itertools import cycle
 
 def alert(website):
   print(f"ALERT!!! -> {website['url']}")
 
+proxies = ['51.159.5.133:3128']
+proxy_pool = cycle(proxies)
+proxy = next(proxy_pool)
+
+#proxy_support = urllib.request.ProxyHandler({'http' : proxy, 'https': proxy})
+#opener = urllib.request.build_opener(proxy_support)
+#urllib.request.install_opener(opener)
+
 async def scrape(website):
   while (True):
-    content = subprocess.run([
-      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-      "--headless",
-      "--disable-gpu",
-      "--dump-dom",
-      website['url']], capture_output=True, text=True).stdout
+    content = ""
+    if 'render' in website and website['render']:
+      content += subprocess.run([
+        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        "--headless",
+        "--disable-gpu",
+        "--dump-dom",
+        website['url']], capture_output=True, text=True).stdout
+    else:
+      resource = urllib.request.urlopen(website['url'])
+      content = resource.read().decode(resource.headers.get_content_charset())
 
     if 'excludes' in website and website['excludes'] not in content:
       alert(website)
