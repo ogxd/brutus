e89e44c29ce2c40c0f78b9fc581220f0ed1ea436
Olivier Giniaux <oginiaux@gmail.com>
2021-11-27 23:03:17 +0100

    Change for C#...

---
+++ brutus/Job.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace brutus
+{
+    public class Job
+    {
+        public string url;
+        public string exludes;
+        public string includes;
+        public int delay = 5000;
+        public event Action<Job> Triggered;
+        public Exception LastError { get; private set; }
+        public bool Paused { get; private set; }
+
+
+        private CancellationTokenSource _cts;
+        private WebClient _client;
+
+        public Job()
+        {
+            _client = new WebClient();
+
+            Start();
+        }
+
+        public void Start()
+        {
+            if (_cts != null && !_cts.IsCancellationRequested)
+                throw new Exception("job is already started");
+
+            _cts = new CancellationTokenSource();
+
+            StartInternalAsync();
+        }
+
+        private async Task StartInternalAsync()
+        {
+            while (!_cts.IsCancellationRequested)
+            {
+                try
+                {
+                    var content = await _client.DownloadStringTaskAsync(new Uri(url));
+
+                    if (!string.IsNullOrEmpty(includes) && !content.Contains(includes))
+                    {
+                        Pause();
+                        Triggered?.Invoke(this);
+                    }
+
+                    if (!string.IsNullOrEmpty(exludes) && content.Contains(exludes))
+                    {
+                        Pause();
+                        Triggered?.Invoke(this);
+                    }
+                }
+                catch(Exception err)
+                {
+                    LastError = err;
+                }
+
+                await Task.Delay(delay);
+            }
+        }
+
+        public void Pause()
+        {
+            if (_cts.IsCancellationRequested)
+                throw new Exception("job is already paused");
+
+            _cts.Cancel();
+        }
+    }
+}
---
+++ brutus/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace brutus
+{
+    class Program
+    {
+        private const string TOKEN = "XXX";
+        private const ulong NOTIFICATION_CHANNEL_ID = 913921054177624188;
+
+        private static ConcurrentDictionary<string, Job> jobs;
+        private static DiscordSocketClient client;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("starting");
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            jobs = new ConcurrentDictionary<string, Job>();
+
+            client = new DiscordSocketClient();
+
+            Console.WriteLine("login in");
+
+            await client.LoginAsync(TokenType.Bot, TOKEN);
+
+            Console.WriteLine("starting");
+
+            await client.StartAsync();
+
+            Console.WriteLine("running !");
+
+            client.MessageReceived += OnMessageReceived;
+
+            await Task.Delay(Timeout.Infinite); // Await until close
+        }
+
+        private static async Task OnMessageReceived(SocketMessage message)
+        {
+            if (!message.Content.StartsWith("!brutus "))
+                return;
+
+            Console.WriteLine("command received");
+
+            try
+            {
+                var split = message.Content.Split(' ');
+                if (split.Length < 2)
+                    throw new Exception("brutus requires at least 1 argument");
+
+                switch (split[1])
+                {
+                    case "status":
+                        await message.Channel.SendMessageAsync("I'm alive! ✨");
+                        break;
+
+                    case "jobs":
+                        if (jobs.Count == 0)
+                        {
+                            await message.Channel.SendMessageAsync($"There are no jobs running. Add a job with !brutus job add MY_JOB_NAME (no spaces)");
+                        }
+                        foreach (var pair in jobs)
+                        {
+                            await message.Channel.SendMessageAsync($"Job '{pair.Key}': {(pair.Value.Paused ? "paused" : "running")} {pair.Value.LastError?.ToString()}");
+                        }
+                        break;
+
+                    case "job":
+                        if (split.Length < 3)
+                            throw new Exception("job requires at least 1 argument");
+
+                        Job job;
+
+                        switch (split[2])
+                        {
+                            case "add":
+                                jobs.TryAdd(split[3], job = new Job());
+                                job.Triggered += Job_Triggered;
+                                break;
+
+                            case "remove":
+                                jobs.TryRemove(split[3], out job);
+                                break;
+
+                            case "set":
+                                jobs.TryGetValue(split[3], out job);
+                                switch (split[4])
+                                {
+                                    case "url":
+                                        job.url = split[4];
+                                        break;
+                                    case "includes":
+                                        job.includes = split[4];
+                                        break;
+                                    case "excludes":
+                                        job.exludes = split[4];
+                                        break;
+                                    case "delay":
+                                        job.delay = int.Parse(split[4]);
+                                        break;
+                                }
+                                break;
+
+                            case "pause":
+                                jobs.TryGetValue(split[3], out job);
+                                job.Pause();
+                                break;
+
+                            case "start":
+                                jobs.TryGetValue(split[3], out job);
+                                job.Start();
+                                break;
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                await message.Channel.SendMessageAsync("Command failed: " + ex);
+            }
+        }
+
+        private static void Job_Triggered(Job job)
+        {
+            var channel = client.GetChannel(NOTIFICATION_CHANNEL_ID) as IMessageChannel;
+            channel.SendMessageAsync("ALERT! => " + job.url);
+        }
+    }
+}
