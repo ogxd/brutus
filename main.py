436b22e9eca2dfb5cde044c2eb737fcd18c241ae
Olivier Giniaux <oginiaux@gmail.com>
2021-11-27 22:06:30 +0100

    More stuff done

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os, subprocess, yaml, time, asyncio, sys, requests, urllib
+from discord.ext import commands
 from itertools import cycle
 
-def alert(website):
+channel = {}
+
+async def alert(website):
+  global channel
   print(f"ALERT!!! -> {website['url']}")
+  await channel.send(f"ALERT!!! -> {website['url']}")
 
 proxies = ['51.159.5.133:3128']
 proxy_pool = cycle(proxies)
@@ -12,7 +17,11 @@ proxy = next(proxy_pool)
 #opener = urllib.request.build_opener(proxy_support)
 #urllib.request.install_opener(opener)
 
+jobs = []
+
 async def scrape(website):
+  global jobs
+  jobs.append(website)
   while (True):
     content = ""
     if 'render' in website and website['render']:
@@ -27,17 +36,25 @@ async def scrape(website):
       content = resource.read().decode(resource.headers.get_content_charset())
 
     if 'excludes' in website and website['excludes'] not in content:
-      alert(website)
+      await alert(website)
+      jobs.remove(website)
       return
 
     if 'includes' in website and website['includes'] in content:
-      alert(website)
+      await alert(website)
+      jobs.remove(website)
       return
 
     await asyncio.sleep(5 if 'delay' not in website else website['delay'])
 
 async def main():
   config = {}
+
+  #await client.wait_until_ready()
+  global channel
+  channel = client.get_channel(id=913921054177624188)
+
+  print(f"Discord channel: {channel}")
 
   with open("config.yaml") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
@@ -50,4 +67,15 @@ async def main():
   for task in tasks:
     await task
 
-asyncio.run(main())
+#---
+bot = commands.Bot(command_prefix='$')
+
+@bot.command()
+async def status(ctx):
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  await main()
+
+client.run('OTEzOTEzODkyNjkyOTcxNTcw.YaFaow._yESra6Hd2PwxfOH75Xw8RQknKo')
